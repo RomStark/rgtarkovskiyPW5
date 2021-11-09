acaf42a340e3ec678147a24b7d044693f7e3b696
RomaNoob <72168932+RomStark@users.noreply.github.com>
2021-11-09 18:51:32 +0300

    Add files via upload

---
+++ rgtarkovskiyPW5/rgtarkovskiyPW5Tests/rgtarkovskiyPW5Tests.swift
@@ -0,0 +1,33 @@
+//
+//  rgtarkovskiyPW5Tests.swift
+//  rgtarkovskiyPW5Tests
+//
+//  Created by Al Stark on 09.11.2021.
+//
+
+import XCTest
+@testable import rgtarkovskiyPW5
+
+class rgtarkovskiyPW5Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
